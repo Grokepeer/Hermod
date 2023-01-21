b08c3aaf898affb198d313334c33e870202aa384
Grokepeer <matteo.minardi2014@gmail.com>
2023-01-21 02:20:39 +0100

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+Hermod
+High-Performance In-Memory Database solution for YBD. Runs on port 2088.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    println!("[Hermod] Up and running...");
+
+    let listener = TcpListener::bind("0.0.0.0:2088").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
