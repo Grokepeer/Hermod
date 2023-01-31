674fe0275e5bd3fdd22dacde74aa9b38463e8734
Grokepeer <matteo.minardi2014@gmail.com>
2023-01-31 23:38:41 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{prelude::*},
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
@@ -22,39 +22,42 @@ fn main() {
         });
     }
 
-    println!("Shutting down.");
+    println!("[Hermod] Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     //Saves in a buffer the request from the TCPStream buffer and then saves its line in a vector names req.
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
+    let rawreq = stream.read(&mut buffer).unwrap();
 
-    let rawreq = String::from_utf8_lossy(&buffer[..]);
-    let req = rawreq.replace("\0", "");
+    // let buffer = BufReader::new(&mut stream);
+
+    // let rawreq = String::from(buffer);
+    let req = rawreq;
 
     // Collects all lines from the http request
-    let reqlines: Vec<_> = req
-    .lines()
-    .collect();
+    // let reqlines: Vec<_> = req
+    // .lines()
+    // .collect();
 
     let test = Json::from("Gotcha");
-    println!("Result of JSON.read(): {}", test.find("id"));
+    println!("Result of JSON.read(): {}", String::from(buffer));
 
     println!("{:?}", req);
     
     // Finds the request line with the Content-Length and saves it in the var size..
-    let split: Vec<_> = req.split("\r\n\r\n").collect();
+    // let split: Vec<_> = req.split("\r\n\r\n").collect();
 
-    println!("{:?}", split[1].replace("\0", ""));
+    // println!("{:?}", split[1].replace("\0", ""));
 
     // Matches the header of the http request
-    let (status, res_content) = match reqlines[0] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "Got it"),
-        "GET /sleep HTTP/1.1" => ("HTTP/1.1 200 OK", "Sleep"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404")
-    };
+    // let (status, res_content) = match reqlines[0] {
+    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "Got it"),
+    //     "GET /sleep HTTP/1.1" => ("HTTP/1.1 200 OK", "Sleep"),
+    //     _ => ("HTTP/1.1 404 NOT FOUND", "404")
+    // };
     
+    let (status, res_content) = ("HTTP/1.1 200 OK", "Got it");
     let length = res_content.len();
     let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{res_content}");
     stream.write_all(response.as_bytes()).unwrap();
